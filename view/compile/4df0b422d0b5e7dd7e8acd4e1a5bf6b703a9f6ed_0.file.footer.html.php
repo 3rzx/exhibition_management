aaf89d16942e4ea7d83888a60a2d7970d0f69b59
3333rrrr <3333rrrr@gmail.com>
2017-04-12 20:06:17 +0800

    First commit and perserve it.

---
+++ view/compile/4df0b422d0b5e7dd7e8acd4e1a5bf6b703a9f6ed_0.file.footer.html.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 3.1.28, created on 2017-04-09 17:16:04
+  from "/var/www/html/web/view/common/footer.html" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.28',
+  'unifunc' => 'content_58e9fbd43186b5_84557912',
+  'file_dependency' => 
+  array (
+    '4df0b422d0b5e7dd7e8acd4e1a5bf6b703a9f6ed' => 
+    array (
+      0 => '/var/www/html/web/view/common/footer.html',
+      1 => 1473736768,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_58e9fbd43186b5_84557912 ($_smarty_tpl) {
+?>
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0.5
+    </div>
+    <strong>Copyright &copy; 2016-2020 <a href="#">ITRI</a>.</strong> All rights reserved.
+</footer>
+<?php }
+}
